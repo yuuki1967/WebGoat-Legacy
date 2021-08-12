003dc8eb812606a25631555440d1edf372cd5cd1
Yuuki Endo <yuuki.endo@checkmarx.com>
2021-08-12 17:26:55 +0900

    Commented out again.

--- src/main/java/org/owasp/webgoat/LessonSource.java
+++ src/main/java/org/owasp/webgoat/LessonSource.java
@@ -68,10 +68,10 @@ public class LessonSource extends HammerHead {
         String source = null;
 
         try {
-		System.out.println( "Entering doPost: " );
-                System.out.println( " - request " + request);
-                System.out.println( " - principle: " + request.getUserPrincipal()
-            );
+		// System.out.println( "Entering doPost: " );
+                // System.out.println( " - request " + request);
+                // System.out.println( " - principle: " + request.getUserPrincipal()
+           //  );
             setCacheHeaders(response, 0);
             WebSession session = (WebSession) request.getSession(true).getAttribute(WebSession.SESSION);
             // FIXME: Too much in this call.
