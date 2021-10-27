4af341074cdbfd4a87bfb3d97ce414c2381e873a
yuuki1967 <yuki06.endo@gmail.com>
2021-10-27 12:06:36 +0900

    Update BackDoors.java
    エンコードするように変更

--- src/main/java/org/owasp/webgoat/lessons/BackDoors.java
+++ src/main/java/org/owasp/webgoat/lessons/BackDoors.java
@@ -22,6 +22,7 @@ import org.apache.ecs.html.TR;
 import org.apache.ecs.html.Table;
 import org.owasp.webgoat.session.DatabaseUtilities;
 import org.owasp.webgoat.session.WebSession;
+import org.owasp.webgoat.util.HtmlEncoder;
 
 /***************************************************************************************************
  * 
@@ -86,23 +87,25 @@ public class BackDoors extends SequentialLessonAdapter
             {
                 Table t = new Table(0).setCellSpacing(0).setCellPadding(0).setBorder(1);
 		
-                TR tr = new TR();
-		tr.addElement(new TD(rs.getString("userid")));
-                tr.addElement(new TD(rs.getString("password")));
-                tr.addElement(new TD(rs.getString("ssn")));
-                tr.addElement(new TD(rs.getString("salary")));
-                tr.addElement(new TD(rs.getString("email")));
-                t.addElement(tr);
+                TR tr = new TR();				
+		HtmlEncoder en = new HtmlEncoder();
+                tr.addElement(new TD(en.encode(rs.getString("userid"))));
+                tr.addElement(new TD(en.encode(rs.getString("password"))));
+                tr.addElement(new TD(en.encode(rs.getString("ssn"))));
+                tr.addElement(new TD(en.encode(rs.getString("salary"))));
+                tr.addElement(new TD(en.encode(rs.getString("email"))));
+                tr.addElement(tr);
                 
                 while (rs.next())
                 {
                     tr = new TR();
-                　　tr.addElement(new TD(rs.getString("userid")));
-                　　tr.addElement(new TD(rs.getString("password")));
-                　　tr.addElement(new TD(rs.getString("ssn")));
-                　　tr.addElement(new TD(rs.getString("salary")));
-                　　tr.addElement(new TD(rs.getString("email")));
-		  　t.addElement(tr);
+		    HtmlEncoder en = new HtmlEncoder();
+                    tr.addElement(new TD(en.encode(rs.getString("userid"))));
+                    tr.addElement(new TD(en.encode(rs.getString("password"))));
+                    tr.addElement(new TD(en.encode(rs.getString("ssn"))));
+                    tr.addElement(new TD(en.encode(rs.getString("salary"))));
+                    tr.addElement(new TD(en.encode(rs.getString("email"))));
+		    tr.addElement(tr);
                 }
                 ec.addElement(t);
             }
